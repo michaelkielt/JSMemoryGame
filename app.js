ced6137ba6d5f84f2dd4b545b262610e81a02748
40156896 <michaelkielt@gmail.com>
2023-06-21 18:17:30 +0100

    created cardArray and linked img paths to objects

---
+++ app.js
@@ -0,0 +1,34 @@
+const cardArray = [
+    {
+        name :'jolteon',
+        img : 'images/jolteon.png',
+    },
+    {
+        name :'lapras',
+        img : 'images/lapras.png',
+    },
+    {
+        name :'pikachu',
+        img : 'images/pikachu.png',
+    },
+    {
+        name :'poliwrath',
+        img : 'images/poliwrath.png',
+    },
+    {
+        name :'snorlax',
+        img : 'images/snorlax.png',
+    },
+    {
+        name :'wartortle',
+        img : 'images/wartortle.png',
+    },
+    {
+        name :'white',
+        img : 'images/white.png',
+    },
+    {
+        name :'blank',
+        img : 'images/blank.png',
+    },
+]
