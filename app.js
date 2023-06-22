48d9842d954e32a03743ae709b4ce3b43bcffdce
40156896 <michaelkielt@gmail.com>
2023-06-22 18:34:30 +0100

    added createBoard function

--- app.js
+++ app.js
@@ -24,11 +24,40 @@ const cardArray = [
         img : 'images/wartortle.png',
     },
     {
-        name :'white',
-        img : 'images/white.png',
+        name :'jolteon',
+        img : 'images/jolteon.png',
     },
     {
-        name :'blank',
-        img : 'images/blank.png',
+        name :'lapras',
+        img : 'images/lapras.png',
     },
-]
+    {
+        name :'pikachu',
+        img : 'images/pikachu.png',
+    },
+    {
+        name :'poliwrath',
+        img : 'images/poliwrath.png',
+    },
+    {
+        name :'snorlax',
+        img : 'images/snorlax.png',
+    },
+    {
+        name :'wartortle',
+        img : 'images/wartortle.png',
+    },
+    
+];
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', id)
+    }
+}
