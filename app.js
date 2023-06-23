61297133f48710eaaa9195be7910285112d1333f
40156896 <michaelkielt@gmail.com>
2023-06-23 11:56:50 +0100

    additional logic to checkMatch for different cases

--- app.js
+++ app.js
@@ -53,8 +53,10 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
-const cardsChosen = [];
-const cardsChosenIds = [];
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = []; 
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -70,14 +72,34 @@ createBoard();
 
 function checkMatch() {
    const cards = document.querySelectorAll('img')
+   const optionOneId = cardsChosenIds[0];
+   const optionTwoId = cardsChosenIds[1];
+
+   if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You have cliked the same image!')
+   }
 
     console.log('check for match!')
     if (cardsChosen[0] == cardsChosen[1]) {
         alert('You have a match!')
-        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
-        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
-        cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
-        cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry try again!')
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = [];
+    cardsChosenIds = [];
+
+    if (cardsWon.length == cardArray.length/2) {
+        resultDisplay.innerHTML = 'Congratulations you caught all the mons!'
     }
 }
 
