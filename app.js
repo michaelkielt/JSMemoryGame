84ffe3a577b3d78144c2ef1e229e1e6618e2c81f
40156896 <michaelkielt@gmail.com>
2023-06-23 11:33:12 +0100

    added checkMatch function

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
 const cardsChosen = [];
+const cardsChosenIds = [];
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -67,11 +68,28 @@ function createBoard() {
 
 createBoard();
 
+function checkMatch() {
+   const cards = document.querySelectorAll('img')
+
+    console.log('check for match!')
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You have a match!')
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
+        cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+    }
+}
+
 function flipCard() {
     console.log(cardArray)
     const cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
     console.log('clicked', cardId)
     console.log(cardsChosen)
     this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length == 2) {
+        setTimeout(checkMatch, 500)
+    }
 };
